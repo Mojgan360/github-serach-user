45b8060d5982ce1820993fb00cc85a88b5ed5902
Mojgan360 <mojgan360@gmail.com>
2020-09-21 00:29:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Route>
+        {' '}
+        <Dashboard></Dashboard>
+      </Route>
+      <Route>
+        {' '}
+        <Login />
+      </Route>
+
+      <Error />
+    </Router>
+  )
+}
+
+export default App
