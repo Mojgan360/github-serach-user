7a198ebbc33bc363667d3c594b6afea267e1386d
Mojgan360 <mojgan360@gmail.com>
2020-09-21 14:21:20 +0200

    added Repo, Flowwes,Flowwing, Gists

--- src/context/context.js
+++ src/context/context.js
@@ -7,8 +7,12 @@ import axios from 'axios'
 const rootUrl = 'https://api.github.com'
 const GithubContext = React.createContext()
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setFollowers] = useState(mockFollowers)
+
   return (
-    <GithubContext.Provider value={'hello from context'}>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
       {children}
     </GithubContext.Provider>
   )
