fec02c95c846175094e2e0e17f30b94ffcfb6ae4
Mojgan360 <mojgan360@gmail.com>
2020-10-02 14:07:05 +0200

    Added username in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,11 @@ const Navbar = () => {
   return (
     <Wrapper>
       {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
       <button
         onClick={() => {
           logout({ returnTo: window.location.origin })
